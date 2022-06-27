1b476ce62a85d33c6d09151cba1a8410190d2260
胡文凡 <wenfan.hu@infaith.com.cn>
2022-06-27 18:55:45 +0800

    up

---
+++ app/src/main/java/com/example/tmp/ButtonWrapper.java
@@ -0,0 +1,28 @@
+package com.example.tmp;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.Button;
+
+@SuppressLint("AppCompatCustomView")
+public class ButtonWrapper extends Button {
+    public ButtonWrapper(Context context) {
+        this(context, null);
+    }
+
+    public ButtonWrapper(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ButtonWrapper(Context context, AttributeSet attrs, int defStyle) {
+        this(context, attrs, defStyle, 0);
+    }
+
+    public ButtonWrapper(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        Log.d("debug", "(Context context, AttributeSet attrs, int defStyle) called");
+        Log.i("debug", "defStyleAttr: " + defStyleAttr + ", defStyleRes: " +  defStyleRes + ", the real style: " + R.style.ButtonTest);
+    }
+}
